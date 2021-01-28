637a57bf482bd724c49e2a3f61bfc72d923a0a12
yeonghoo2 <yeonghoon2@gmail.com>
2021-01-28 12:56:12 +0900

    create springboot

---
+++ springboot/demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello, World";
+    }
+
+    @RequestMapping("/test")
+    public @ResponseBody String test() {
+        return "Hello, test";
+    }
+}
