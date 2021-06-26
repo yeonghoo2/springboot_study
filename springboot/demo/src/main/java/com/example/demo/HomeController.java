ba2229de6a678e9d7542277deb0bdbc6d747b8df
yeonghoo2 <yeonghoon2@gmail.com>
2021-06-26 16:24:04 +0900

    set green

--- springboot/demo/src/main/java/com/example/demo/HomeController.java
+++ springboot/demo/src/main/java/com/example/demo/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, World -- blue";
+        return "Hello, World -- green";
     }
 
     @RequestMapping("/test")
